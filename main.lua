3da5741754579f881adb5749ad60c4c00f238c15
Bruno Richard <bruno683@outlook.fr>
2022-09-14 14:02:13 +0200

    Added pad and ball objects

--- main.lua
+++ main.lua
@@ -1,22 +1,30 @@
 
 
 love.graphics.setDefaultFilter("nearest", "nearest")
-local push = require "push"
-_WINDOW_WIDTH = 1920
-_WINDOW_HEIGHT = 1080
+
+_WINDOW_WIDTH = 1280
+_WINDOW_HEIGHT = 980
+--pads properties
+local padWidth = 15
+local padHeight = 40
+--ball properties
+local ball = {}
+ball.radius = 10
+ball.color = love.graphics.setColor(0, 1, 0, 1)
+ball.x = _WINDOW_WIDTH/2 - ball.radius
+ball.y = _WINDOW_HEIGHT/2 - ball.radius
 
 
-VIRTUAL_WIDTH = 960
-VIRTUAL_HEIGHT = 540
+
 
 
 function love.load()
 
 
-  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, _WINDOW_WIDTH, _WINDOW_HEIGHT,{
-    fullscreen = false,
-    resizable = false,
-    vsync = true
+  love.window.setMode(_WINDOW_WIDTH, _WINDOW_HEIGHT, {
+      fullscreen = false,
+      resizable = false,
+      vsync = true
   })
 end
 
@@ -29,8 +37,16 @@ end
 
 function love.draw()
   love.graphics.clear(0,0,0,1)
-  love.graphics.rectangle('fill', 10, 60, 5, 20)
 
+
+  --set color
+  love.graphics.setColor(1, 1, 1, 1)
+  --render first pad
+  love.graphics.rectangle('fill', 15, 30, padWidth, padHeight)
+  --render second pad
+  love.graphics.rectangle('fill', _WINDOW_WIDTH - 30, _WINDOW_HEIGHT - 70, padWidth, padHeight)
+  -- render the ball
+  love.graphics.circle('fill', ball.x, ball.y, ball.radius, ball.color)
 
 
   --love.graphics.printf("Hello Pong !",
